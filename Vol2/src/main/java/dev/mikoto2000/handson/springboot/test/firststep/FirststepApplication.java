37eb9afb0b9a10dac071d3bb3af13538f9593f9c
mikoto2000 <mikoto2000@gmail.com>
2026-02-19 07:55:18 +0900

    Vol2 のプロジェクトテンプレートを追加。

---
+++ Vol2/src/main/java/dev/mikoto2000/handson/springboot/test/firststep/FirststepApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.handson.springboot.test.firststep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirststepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirststepApplication.class, args);
+	}
+
+}
